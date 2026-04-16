c6860c33732769841e6f99ed17d52a4d36abc407
Vishal.R.Nadagoudar <137093870+VishalRN@users.noreply.github.com>
2026-04-16 11:24:46 +0530

    yes

--- 42 - Enumerate Function in Python/main.py
+++ 42 - Enumerate Function in Python/main.py
@@ -7,7 +7,11 @@ marks = [12, 56, 32, 98, 12,  45, 1, 4]
 #     print("Harry, awesome!")
 #   index +=1
 
+# The enumerate function is a built-in function in Python that allows u loop over an sequence(list,tuple,string) and get the index and value at the same time.
 for index, mark in enumerate(marks, start=1):
   print(mark)
   if(index == 3):
     print("Harry, awesome!")
+
+for index, mark in enumerate(marks, start=1):
+  print(index, mark)
--- 43 - Virtual Environment in Python/main.py
+++ 43 - Virtual Environment in Python/main.py
@@ -1,2 +1,3 @@
+# A virtual environment is a tool used to create an isolated Python environment on a single machine. It allows you to work on multiple projects with different dependencies and packages without conflicts between them.
 import pandas as pd
 print(pd.__version__)
